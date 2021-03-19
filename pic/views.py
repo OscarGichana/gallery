2d62c5c1049f43d24da87476261d3bd01328487b
Oscar Gichana <ossygicchy@gmail.com>
2021-03-19 16:18:23 +0300

    adding views to app

--- pic/views.py
+++ pic/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse
+import datetime as dt
+from django.http  import HttpResponse,Http404
+from .models import Picture
 
 # Create your views here.
+
+def pic(request):
+    date = dt.date.today()
+    pic = Picture.ft_pic()
+    return render(request, 'pic.html', {"date": date,"pic":pic})
+
+
+def picture(request,picture_id):
+    try:
+        picture = Picture.objects.get(id = picture_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"article.html", {"picture":picture})
