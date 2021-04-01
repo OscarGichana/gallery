11fe5eefd334579f5820493986964894f658842d
Oscar Gichana <ossygicchy@gmail.com>
2021-04-01 11:49:39 +0300

    push to heroku

--- Procfile
+++ Procfile
@@ -2,3 +2,4 @@ release: python manage.py makemigrations
 release: python manage.py migrate
 
 web: gunicorn gallery.wsgi
+
--- gallery/settings.py
+++ gallery/settings.py
@@ -23,29 +23,17 @@ from django.core.wsgi import get_wsgi_application
 #  application = DjangoWhiteNoise(application)
 
 # development
-if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
-       }
-       
-   }
-
-# production
-else:
-   DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
+    }
+    
+}
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
